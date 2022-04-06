a3268e0b95c1d95438c82991383e84f4c31f433b
diego.ribeiro <dmribeiro87@gmail.com>
2022-04-06 10:56:31 -0300

    Init actions

--- app/src/main/java/com/example/testecomponentizacao/domain/repo/ProductRepository.kt
+++ app/src/main/java/com/example/testecomponentizacao/domain/repo/ProductRepository.kt
@@ -4,6 +4,7 @@ import com.example.testecomponentizacao.domain.model.Product
 
 
 interface ProductRepository {
+
     suspend fun getProducts(): List<Product>
     suspend fun insertProducts(products: List<Product>)
     suspend fun deleteAllProducts()
